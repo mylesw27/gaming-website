363408f62e3a225b6ff6ae2d970c333754e651ae
juanedcabrera <juanedcabrera@gmail.com>
2023-06-09 11:18:44 -0400

    Updated some New and Random game sections for fetching games

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,15 +1,20 @@
 'use client'
 import React from 'react';
 import Navigation from '../components/Navigation/page'
+import Random from '../components/Random/page';
+import NewGames from '../components/NewGames/page';
+
+// Grab games from API and 
 
 export default function Landing() {
-
   return (
     <div>
       <div>
         <Navigation />
         <h1>Discover Amazing Designs</h1>
         <p>Be inspired by talented designers around the world.</p>
+        <Random />
+        <NewGames />
       </div>
       <footer>
         <p>© 2023 Website</p>
