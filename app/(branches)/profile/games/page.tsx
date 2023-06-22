dda8e9ccd8504d6799b2917a547aa693e45c7426
juanedcabrera <juanedcabrera@gmail.com>
2023-06-22 12:03:44 -0600

    Profile now working

--- app/(branches)/profile/games/page.tsx
+++ app/(branches)/profile/games/page.tsx
@@ -1,13 +1,18 @@
 import Navigation from '../../../components/Navigation/page';
 
-const AllGames = () => {
+// List of games that the user has uploaded
+// This page will be a list of games that the user has uploaded
+// Under each game will be a link to edit the game
+
+
+const UserGames = () => {
   return (
     <div>
       <Navigation />
-      <h2>All Games</h2>
+      <h2>Only User Games</h2>
       <a href="/games/edit">Edit</a>
     </div>
   );
 };
 
-export default AllGames;
+export default UserGames;
--- app/(branches)/profile/page.tsx
+++ app/(branches)/profile/page.tsx
@@ -1,13 +1,78 @@
-import Navigation from "../../components/Navigation/page";
+'use client'
+import React from 'react';
+import PasswordReset from '@/app/components/PasswordReset/page';
+import Navigation from '../../components/Navigation/page';
+import jwt from 'jsonwebtoken';
+
+const handlePasswordReset = async (newPassword: string) => {
+  try {
+    // Get the user ID from the jwt payload
+    const token = localStorage.getItem('token');
+    if (!token) {
+      console.log('Token not found.');
+      // Handle error scenario, such as redirecting to the login page
+      return;
+    }
+
+    const decodedToken = jwt.decode(token);
+    if (!decodedToken || typeof decodedToken !== 'object') {
+      console.log('Decoded token not found or invalid.');
+      // Handle error scenario, such as redirecting to the login page
+      return;
+    }
+
+    const userId = decodedToken.id;
+
+    console.log('User ID from JWT:', userId);
+    console.log('Decoded token:', decodedToken);
+
+    // Make a PUT request to update the user's password
+    const response = await fetch(`http://localhost:8000/api-v1/users/${userId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ password: newPassword }),
+    });
+
+    if (response.ok) {
+      console.log('Password reset successful!');
+      // Perform any additional actions upon successful password reset
+    } else {
+      console.log('Password reset failed.');
+      // Handle error scenario, such as displaying an error message to the user
+    }
+  } catch (error) {
+    console.log('An error occurred during password reset:', error);
+    // Handle error scenario
+  }
+};
 
 const Profile = () => {
-    return (
-      <div>
-        <Navigation />
-        <h2>Profile</h2>
-      </div>
-    );
-  };
-  
-  export default Profile;
-  
+  const token = localStorage.getItem('token');
+  if (!token) {
+    console.log('Token not found.');
+    // Handle error scenario, such as redirecting to the login page
+    return;
+  }
+  const decodedToken = jwt.decode(token);
+  if (!decodedToken || typeof decodedToken !== 'object') {
+    console.log('Decoded token not found or invalid.');
+    // Handle error scenario, such as redirecting to the login page
+    return;
+  }
+
+  console.log(decodedToken)
+
+  return (
+    <div>
+      <Navigation />
+      <h2>Hi {decodedToken.name}</h2>
+      <PasswordReset onSubmit={handlePasswordReset} />
+      {/* List of current games uploaded */}
+      <a href = '/profile/games'>Uploaded Game</a>
+    </div>
+  );
+};
+
+export default Profile;
