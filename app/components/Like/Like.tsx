4ad92bc815ff409cb2d0b5ab49551c9136401341
mylesw27 <myleswiegel27@gmail.com>
2023-08-18 14:44:23 -0700

    update like button to post to db

--- app/components/Like/Like.tsx
+++ app/components/Like/Like.tsx
@@ -1,10 +1,57 @@
 import { useEffect, useState } from "react"
+import jwt_decode from "jwt-decode"
 
-export default function Like(props: {game: any}) {
-    const [game, setGame] = useState(props.game)
+interface Game {   
+    _id: string;
+  title: string;
+  userName: string;
+  category: string;
+  description: string;
+  image: string;
+  techstack: string;
+  github: string;
+  link: string;
+  likes: number;
+  comments: number;
+  views: number;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+interface User {   
+    email: string;
+    exp: number;
+    iat: number;
+    id: string;
+    name: string;
+    userName: string;
+}
+
+export default function Like(props: {game: Game}) {
+    const [game, setGame] = useState(props.game._id)
+    const [user, setUser] = useState<User | null>(null)
+    const [time, setTime] = useState<any>(null)
+    const token = localStorage.getItem("token")
+
+    useEffect(() => {
+        if (token) {
+            const decoded: User = jwt_decode(token)
+            setUser(decoded)
+        }
+        setTime(new Date().getTime())
+    }, [])
 
     const submitLike = () => {
-        console.log(game._id)
+        console.log(game, user.id, time)
+        fetch(`http://localhost:8000/api-v1/like/`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: token,
+            },
+            body: JSON.stringify({user_id: user.id, game_id: game, time: time})
+        })
+
     }
 
     return (
