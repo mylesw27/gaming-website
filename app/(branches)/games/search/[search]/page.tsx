99b66da91054d2ddba0ff06458aea2c5d9d40a9f
juanedcabrera <juanedcabrera@gmail.com>
2023-08-23 16:36:32 -0600

    Updated the search page with same look at Categories

---
+++ app/(branches)/games/search/[search]/page.tsx
@@ -0,0 +1,61 @@
+'use client'
+import { useEffect, useState } from "react";
+
+interface Game {
+    _id: string;
+    title: string;
+    userName: string;
+    category: string;
+    description: string;
+    image: string;
+    likes: number;
+    comments: number;
+    views: number;
+    createdAt: Date;
+    updatedAt: Date;
+  }
+
+export default function Search({ params }: { params: { search: string } }) {
+    const [search, setSearch] = useState('');
+    const [games, setGames] = useState<Game[]>([]);
+
+    useEffect(() => {
+        async function searchGames() {
+            try {
+                const response = await fetch(`http://localhost:8000/api-v1/game/search/${params.search}`);
+                if (!response.ok) {
+                    throw new Error(`Failed to search for game: ${params.search}`);
+                }
+                const data = await response.json();
+                setGames(data.games);
+                console.log(data);
+            } catch (error) {
+                console.error('Error searching for game:', error);
+            }
+        }
+        searchGames();
+    }, []); 
+
+    return (
+        <div className="text-slate-300 bg-gray-800 py-9">
+        <h1 className="text-2xl font-bold mb-4 md:text-3xl md:mb-6 text-center">
+          Search Results: {params.search}
+        </h1>
+        <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+          {games.length > 0 ? (
+            games.map((game, index) => (
+              <a key={index} href={`/games/${game._id}`} className="block">
+                <div className="p-4 text-slate-300 bg-gray-700 rounded-lg">
+                  <img src={game.image} alt={game.title} className="object-cover w-full h-64 md:h-80 lg:h-96 rounded-lg" />
+                  <p className="text-lg font-bold mb-2 md:text-xl">{game.title}</p>
+                  <p className="text-sm text-slate-300 mb-2 md:text-base">Category: {game.category}</p>
+                </div>
+              </a>
+            ))
+          ) : (
+            <div className="text-red-500">No games found.</div>
+          )}
+        </div>
+      </div>
+    );
+}
