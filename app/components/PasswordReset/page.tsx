2dcfc7b9eb78aa6d039227641ce92eda77dd5869
juanedcabrera <juanedcabrera@gmail.com>
2023-08-25 15:10:05 -0600

    Updated the profile page - still needs work

--- app/components/PasswordReset/page.tsx
+++ app/components/PasswordReset/page.tsx
@@ -14,21 +14,29 @@ const PasswordReset: React.FC<PasswordResetProps> = ({ onSubmit }) => {
   };
 
   return (
-    <div>
-      <h2>Reset Your Password</h2>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="newPassword">New Password:</label>
+    <div className="p-6">
+      <h2 className="text-2xl font-semibold mb-4">Reset Your Password</h2>
+      <form onSubmit={handleSubmit} className="space-y-4">
+        <label htmlFor="newPassword" className="block font-medium text-gray-700">
+          New Password:
+        </label>
         <input
           id="newPassword"
           type="password"
           placeholder="Enter your new password"
           value={newPassword}
           onChange={(e) => setNewPassword(e.target.value)}
+          className="border rounded py-2 px-3 w-full focus:outline-none focus:ring focus:border-blue-500"
         />
-        <button type="submit">Reset Password</button>
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded"
+        >
+          Reset Password
+        </button>
       </form>
     </div>
   );
-};
+}  
 
 export default PasswordReset;
